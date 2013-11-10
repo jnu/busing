4b5c621e35df50570b030002baec3d7aff3b09b9
Joseph Nudell <joenudell@gmail.com>
2013-11-10 11:51:01 -0600

    update doc

--- scr/compile_data.py
+++ scr/compile_data.py
@@ -6,29 +6,41 @@ $ python compile_data.py [-u <user> [-p <password>]]
 import argparse
 import os
 import csv
-import re
 # local
 from access import *
 
 
 # Globals
 DATA_PATH = "../data/"
-t = None
+
 
 # Functions
 def get_csv_headers(schema):
+    '''
+    Get the titles to be used as CSV headers per schema
+    '''
     col_map = schema['col_map']
     return col_map.values()
 
 def get_spreadsheet_headers(schema):
+    '''
+    Get which headers are desired per schema
+    '''
     col_map = schema['col_map']
     return col_map.keys()
 
 def clean_headers(schema):
+    '''
+    Get headers that will match what Google names them in the ListFeed
+    '''
     headers = get_spreadsheet_headers(schema)
-    return [re.sub(r'[^\w\d\-_\.]', '', h).lower() for h in headers]
+    return [clean_cell(h) for h in headers]
 
 def create_csv_from_schema(schema, client):
+    '''
+    Create a list of CSV rows from the Spreadsheet based on schema,
+    connecting through client
+    '''
     key = schema['gd_key']
 
     lines = [get_csv_headers(schema)]
@@ -39,7 +51,6 @@ def create_csv_from_schema(schema, client):
     for i, row in enumerate(rows):
         line = []
 
-        # pull line data, using pull_cols as guide
         for header in headers:
             line.append(row.custom[header].text)
 
@@ -54,15 +65,19 @@ if __name__=='__main__':
     setup_access_cli(parser)
     args = parser.parse_args()
 
+    # set up access
     client = GoogleIntegration(args.user, args.password, 'spreadsheet').connect()
 
     schema = parse_schema('schema.json')
 
+    # create CSV from spreadsheet per schema
     csv_lines = create_csv_from_schema(schema, client)
 
+    # make sure paths exist
     if not os.path.exists(DATA_PATH):
         os.makedirs(DATA_PATH)
-        
+
+    # save CSV locally
     path = ('%s/%s' % (DATA_PATH, schema['name'])).replace('//', '/')
     with open(path, 'w') as fh:
         writer = csv.writer(fh)

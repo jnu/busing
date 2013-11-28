ef1d86f335502fed4ee8da49c2a4c84ef0988497
Joseph Nudell <joenudell@gmail.com>
2013-11-28 13:42:35 -0500

    update styles, add map

--- js/busvis.js
+++ js/busvis.js
@@ -5,9 +5,14 @@
 
 
 // helpers
-function dot(key) {
+function dot() {
+    var args = arguments;
     return function(d) {
-        return d[key];
+        function _dot(v, args) {
+            if (!(args && args.length)) return v;
+            else return _dot(v[args[0]], Array.prototype.slice.call(args, 1));
+        }
+        return _dot(d, args);
     };
 }
 
@@ -17,7 +22,42 @@ function val(key) {
     };
 }
 
+function nest(f) {
+    return function(d) {
+        return f(d);
+    };
+}
 
+function fixWidth(selector) {
+    var selection = d3.selectAll(selector);
+    var node = selection.node();
+    var $node = $(node);
+    var $parent = $node.parent();
+
+    return function() {
+        $node.hide();
+        var targetWidth = $parent.width();
+        $node.width(targetWidth).show();
+        $node.trigger({
+            type: 'update',
+            width: targetWidth,
+            height: $node.height()
+        });
+    };
+} 
+
+function project(projector) {
+    var projection = projector();
+    return function(chart, width, height) {
+        var node = chart.anchor();
+        var $node = $(node);
+        width = (width === undefined) ? $node.parent().width() : width;
+        height = (height === undefined) ? chart.height() : height;
+
+        return chart
+            .projection(projection.translate([width / 2, height / 2]));
+    };
+}
 
 
 
@@ -26,51 +66,64 @@ function val(key) {
  * @param  {Error} error [description]
  * @param  {Array} rows  [description]
  */
-function init() {
+function init(opts) {
     var cf;
     var dims = {};
-    var groups = window.groups = {};
-    var charts = {
-        country: countryChart,
-        year: yearChart
+    var groups = {};
+    // map chart name to [type, dim]
+    var charts = window.charts = {
+        country: [countryChart, 'country'],
+        year: [yearChart, 'year'],
+        map: [worldMap, 'country']
     };
+    var worldJson = opts.worldJson;
+    var $window = $(window);
 
     // helpers
-    function initChart(chartConstructor, key) {
+    function initChart(chartConstructor, key, dim) {
         // dom containers are expected to have proper ids
         var selector = '#vis-' + key;
+        $window.resize(fixWidth(selector));
         return chartConstructor(selector)
-            .dimension(dims[key])
-            .group(groups[key]);
+            .dimension(dims[dim])
+            .group(groups[dim]);
     }
 
     // charts
-    function countryChart(key) {
-        return initChart(dc.rowChart, key)
-            .width(400)
-            .height(200)
+    function countryChart(key, dim) {
+        return initChart(dc.rowChart, key, dim)
             .margins({top: 20, left: 10, right: 10, bottom: 20})
+            .height(250)
             .label(dot('key'))
             .title(dot('value'))
             .elasticX(true)
             .xAxis().ticks(4);
     }
 
-    function yearChart(key) {
-        return initChart(dc.bubbleChart, key)
-            .width(600)
+    function yearChart(key, dim) {
+        return initChart(dc.bubbleChart, key, dim)
             .height(250)
             .x(d3.scale.linear().domain([1900, 2015]))
             .elasticY(true)
             .keyAccessor(dot('key'))
             .radiusValueAccessor(val('population'))
             .valueAccessor(val('count'));
-
     }
 
-    // returns callback 
+    function worldMap(key, dim) {
+        var chart = initChart(dc.geoChoroplethChart, key, dim)
+            .height(400)
+            .overlayGeoJson(worldJson.features, "countries", dot('properties', 'name'));
+        var projector = project(d3.geo.equirectangular);
+        $(chart.anchor()).on('update', function(e) {
+            projector(chart, e.width, e.height);
+        });
+        return projector(chart);
+    }
+
+    // returned function is actually init logic 
     return function(error, rows) {
-        window.rows = rows;
+        // map of crossfilter vars to csv column headers
         var cfVars = {
             country : 'country',
             method : 'fare_method',
@@ -80,11 +133,10 @@ function init() {
         cf = crossfilter(rows);
 
         // create crossfilter dimensions and groups
-        for (var cfVar in cfVars) {
-            var csvCol = cfVars[cfVar];
+        _(cfVars).map(function(csvCol, cfVar) {
             var dim = dims[cfVar] = cf.dimension(dot(csvCol));
             groups[cfVar] = dim.group();
-        }
+        });
 
         // apply special reductions as necessary to groups
         groups.year.reduce(function add(p, v) {
@@ -103,12 +155,14 @@ function init() {
         });
 
         // invoke all charts
-        for (var c in charts) {
-            charts[c] = charts[c](c);
-        }
+        _(charts).map(function(vars, key) {
+            charts[key] = vars[0](key, vars[1]);
+        });
 
         // render
-        dc.renderAll();
+        var doRender = function() { dc.renderAll(); };
+        $window.resize(_.debounce(doRender, 150));
+        doRender();
     };
 }
 
@@ -133,5 +187,11 @@ function formatRows(d) {
 
 // init
 $(function() {
-    d3.csv('../data/busing.csv', formatRows, init());
+    d3.json('../data/world.geojson', function(mapData) {
+        var opts = {
+            worldJson: mapData
+        };
+
+        d3.csv('../data/busing.csv', formatRows, init(opts));
+    });
 });
